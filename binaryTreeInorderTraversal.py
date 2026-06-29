162bc402f12f168b26a0c60538534781e2081eed
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-06-29 11:12:31 -0600

    Binary Tree Inorder Traversal #94

---
+++ binaryTreeInorderTraversal.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorderTraversalRecursive(root):
+    route = []
+    def checkChild(node):
+        if node.left is not None:
+            checkChild(node.left)
+
+        route.append(node.val)
+
+        if node.right is not None:
+            checkChild(node.right)
+
+    if root is not None:
+        checkChild(root)
+
+    return route
+
+root = TreeNode(val=1)
+root.left = TreeNode(val=2)
+root.left.left = TreeNode(val=4)
+root.left.right = TreeNode(val=5)
+root.left.right.left = TreeNode(val=6)
+root.left.right.right = TreeNode(val=7)
+root.right = TreeNode(val=3)
+root.right.right = TreeNode(val=8)
+root.right.right.left = TreeNode(val=9)
+
+print(inorderTraversalRecursive(root))
