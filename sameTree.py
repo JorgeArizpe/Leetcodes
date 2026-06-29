3da192d1d7f776a3832ab9f1c42d41e0d052cb56
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-06-29 12:04:07 -0600

    Same Tree #100

---
+++ sameTree.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSameTree(p, q):
+    def inorderTraversalRecursive(root):
+        route = []
+        def checkChild(node):
+            if node.left is not None:
+                checkChild(node.left)
+            else:
+                route.append(None)
+            
+            route.append(node.val)
+    
+            if node.right is not None:
+                checkChild(node.right)
+            else:
+                route.append(None)
+            
+        if root is not None:
+            checkChild(root)
+
+        print(route)
+        return route
+
+    return inorderTraversalRecursive(p) == inorderTraversalRecursive(q)
+
+p = TreeNode(val=1)
+p.left = TreeNode(val=2)
+p.right = TreeNode(val=3)
+q = TreeNode(val=1)
+q.left = TreeNode(val=2)
+q.right = TreeNode(val=3)
+
+print(isSameTree(p, q))
