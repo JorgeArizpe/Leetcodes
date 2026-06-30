9bd9add245724c01070f0fa0d61c34680d68e768
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-06-30 10:17:13 -0600

    Zigzag Conversion #6

---
+++ zigzagConversion.py
@@ -0,0 +1,26 @@
+def convert(s, numRows):
+    if numRows == 1 or numRows >= len(s):
+        return s
+
+    rows = [""] * numRows
+    currRow = 0
+    direction = -1
+
+    for char in s:
+        rows[currRow] += char
+
+        if currRow == 0 or currRow == numRows -1:
+            direction *= -1
+
+        currRow += direction
+
+    return "".join(rows)
+
+s = "PAYPALISHIRING"
+target = "PINALSIGYAHRPI"
+numRows = 4
+
+result = convert(s, numRows)
+print(result)
+
+assert result == target
