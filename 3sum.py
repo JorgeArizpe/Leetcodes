4b52e17be8752417712245d4c3b4ef4fd689d8b5
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-07-02 18:03:02 -0600

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -11,7 +11,38 @@ def threeSum(nums):
                 res.add((nums[i], nums[j], x))
 
     return [list(t) for t in res]
-    
+
+def threeSumGood(nums):
+    nums.sort()
+    res = []
+    n = len(nums)
+
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        l, r = i + 1, n - 1
+
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+
+            if s == 0:
+                res.append([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+
+                while l < r and nums[l] == nums[l - 1]:
+                    l += 1
+                while l < r and nums[r] == nums[r + 1]:
+                    r -= 1
+
+            elif s < 0:
+                l += 1
+            else:
+                r -= 1
+
+    return res
+
 nums = [-1,0,1,2,-1,-4]
 
-print(threeSum(nums))
+print(threeSumGood(nums))
