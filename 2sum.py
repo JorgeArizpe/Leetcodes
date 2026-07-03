27e4e746c7dc37bd4078cfd3b1ba51369930b11e
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-07-02 18:00:49 -0600

    3Sum #15

---
+++ 2sum.py
@@ -0,0 +1,18 @@
+def twoSum(nums, target):
+    numMap = {}
+    n = len(nums)
+
+    for i in range(n):
+        numMap[nums[i]] = i
+
+    for i in range(n):
+        complement = target - nums[i]
+        if complement in numMap and numMap[complement] != i:
+            return [i, numMap[complement]]
+
+    return []
+    
+nums = [-1,0,1,2,-1,-4]
+target = 3
+
+print(twoSum(nums, target))
