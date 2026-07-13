21cf4741c0263865d2e1674b4c68ede20785590e
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-07-12 22:53:59 -0600

    Sequential Digits #1291

---
+++ 1291-sequentialDigits/sequentialDigits.py
@@ -0,0 +1,14 @@
+def sequentialDigits(low, high):
+    numbers = [*range(1,10)]
+
+    for number in numbers:
+        x = number % 10
+        if x < 9:
+            numbers.append(number * 10 + x + 1)
+
+    return [number for number in numbers if low < number < high]
+
+low = 10
+high = 1000000000
+
+print(sequentialDigits(low, high))
