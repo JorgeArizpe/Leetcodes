7d1d0729ad576edf61b7a78120df0533211bcd26
JorgeArizpe <jorgeantonio.arizpe@gmail.com>
2026-08-14 10:32:39 -0600

    Repeated Substring Pattern #459

---
+++ 459-repeatedSubstringPattern/repeatedSubstringPattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+        for i in range(1, n//2 + 1):
+            if n % i == 0:
+                subString = s[:i]
+                if subString * (n//i) == s:
+                    return True
+
+        return False
+
+solution = Solution()
+s = "abab"
+
+print(solution.repeatedSubstringPattern(s))
